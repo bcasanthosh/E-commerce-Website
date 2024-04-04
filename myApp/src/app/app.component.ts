982766f0fccbafea0d686d4a0ceff21a6aea7bdd
unknown <santhoshbca9384@gmail.com>
2024-04-04 10:44:16 +0530

    pro

---
+++ myApp/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { MainComponent } from "./main/main.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent]
+})
+export class AppComponent {
+  title = 'myApp';
+}
